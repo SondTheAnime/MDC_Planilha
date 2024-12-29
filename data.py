5a218ba85aedebfbd33e5cc427abd55d1796c7ed
sondtheanime <joaovictorbrtor2@gmail.com>
2024-12-29 13:50:11 -0300

    Update .gitignore to exclude secrets.toml, refactor data retrieval in data.py to use Streamlit secrets for item descriptions, costs, quantities, and profit margins, and add python-dotenv to requirements.txt for environment variable management.

--- data.py
+++ data.py
@@ -4,20 +4,10 @@ import streamlit as st
 @st.cache_data
 def get_initial_data():
     return {
-        "Descrição do Item": [
-            "Salário Pessoal",
-            "Energia",
-            "Aluguel",
-            "Internet",
-            "Material de Limpeza",
-            "Material de Apoio",
-            "Marketing",
-            "Franquia",
-            "Sistema"
-        ],
-        "Custo Unitário (R$)": [7000.00, 800.00, 2500.00, 150.00, 400.00, 600.00, 250.00, 550.00, 150.00],
-        "Quantidade Mensal": [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        "Margem de Lucro (%)": [30, 30, 30, 30, 30, 30, 30, 30, 30],
+        "Descrição do Item": st.secrets.itens.descricoes,
+        "Custo Unitário (R$)": st.secrets.itens.custos,
+        "Quantidade Mensal": st.secrets.itens.quantidades,
+        "Margem de Lucro (%)": st.secrets.itens.margens,
     }
 
 def process_data(data):
